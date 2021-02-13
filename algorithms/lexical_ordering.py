9b11db7ec56e1fc371836e4a441258fc38498b4a
Mgo <mgotm13@gmail.com>
2021-02-13 18:38:17 +0400

    Lexical Ordering as an algorithm

--- algorithms/lexical_ordering.py
+++ algorithms/lexical_ordering.py
@@ -5,25 +5,30 @@ from util.functions import swap, reversePart
 def lexical_ordering(arr):
     """
         Every time it returns next Lexical order
+        Example
+        v0 = [1, 2, 3]
+        v = lexical_ordering(v0) # [1,3,2]
+        v1 = lexical_ordering(v) # [2,1,3]
+        v2 = lexical_ordering(v1)# [2 3 1]
     """
-    val = np.array(arr)
+    array = np.array(arr)
 
     # Step 1
     largestI = -1
 
-    for i in range(val.size - 1):
-        if val[i] < val[i + 1]:
+    for i in range(array.size - 1):
+        if array[i] < array[i + 1]:
             largestI = i
 
     # Step 2
     largestJ = -1
 
-    for j in range(val.size):
-        if val[largestI] < val[j]:
+    for j in range(array.size):
+        if array[largestI] < array[j]:
             largestJ = j
 
     # Step 3
-    swap(val, largestI, largestJ)
+    swap(array, largestI, largestJ)
 
     # Step 4 reverse from largestI + 1 to the end
-    return reversePart(val, largestI + 1, arrayLength)
+    return reversePart(array, largestI + 1, array.size)
