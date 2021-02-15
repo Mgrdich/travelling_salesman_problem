d18cac5c3207e2ac39acc8993d435ee0b01e2a21
Mgo <mgotm13@gmail.com>
2021-02-15 23:51:58 +0400

    util functions explanations

--- util/functions.py
+++ util/functions.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def swap(arr, i, j):
     """
-    Swaps elements of an array and returns the reference
+        Swaps elements of an array and returns the reference
     """
     arr[i], arr[j] = arr[j], arr[i]
     return arr
@@ -11,6 +11,6 @@ def swap(arr, i, j):
 
 def reversePart(arr, i, j):
     """
-        reverse elements of an array with a Given Indexes
+        Reverse elements of an array with a Given Indexes
     """
     return np.concatenate([arr[0:i], arr[j:i - 1:-1], arr[j + 1:]])
